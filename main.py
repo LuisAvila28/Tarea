40c5382aaca373b286b4e6167528bbb76ab3c0d3
LuisAvila28 <23308060610140@cetis61.edu.mx>
2026-04-14 07:33:45 -0500

    act

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
-def main():
-    print("Hello from tarea!")
+import flet as ft
+from src.controllers.UserController import AuthController
+from src.controllers.TareaController import TareaController
+from src.views.LoginView import LoginView
+from src.views.dashboard import DashboardView
+
+
+def main(page: ft.Page):
+    auth_ctrl = AuthController()
+    task_ctrl = TareaController()
+
+    def route_change(route):
+        page.views.clear()
+        if page.route == "/":
+            page.views.append(LoginView(page, auth_ctrl))
+        elif page.route == "/dashboard":
+            page.views.append(DashboardView(page, task_ctrl))
+        page.update()
+
+    page.on_route_change = route_change
+    page.go("/")
 
 
 if __name__ == "__main__":
-    main()
+    ft.run(main)
